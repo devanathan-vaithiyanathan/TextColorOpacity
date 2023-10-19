89869ad5fb8c0a9253b793d3682bea26b07b9d99
devanathan-vaithiyanathan <114395405+devanathan-vaithiyanathan@users.noreply.github.com>
2023-10-19 14:04:02 +0530

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace TextColorOpacityTesting;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void opacitySlider_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        double opacity = e.NewValue;
+        Color textColor = Colors.Red;
+        Color text = new Color(textColor.Red, textColor.Green, textColor.Blue, (byte)(opacity * 255));
+        myEditor.TextColor = text;
+    }
+}
+
